877b7f46a5d117326ce541332161984c1ec4d99e
ShaQi <shaghay3gh@gmail.com>
2024-02-12 13:46:21 +0100

    add secodn piramid. It is bizzare when secod piramid comes second, it does not get printed

--- piramid.c
+++ piramid.c
@@ -2,12 +2,16 @@
 #include <stdio.h>
 
 void print_piramid(int n);
+void print_another_piramid(int n);
 
 
 int main(void){
     int i = get_int("piramid: ");
+    int j = get_int("second piramid: ");
+    print_another_piramid(j);
     print_piramid(i);
 }
+
 
 void print_piramid(int n){
     for (int i = 0; i < n; i++){
@@ -15,6 +19,20 @@ void print_piramid(int n){
             printf("#");
         }
 
+        printf("\n");
+    }
+}
+
+
+void print_row(int n){
+    for (int i; i < (n+1); i++){
+        printf("#");
+    }
+}
+
+void print_another_piramid(int n){
+    for (int i; i < n; i++){
+        print_row(i);
         printf("\n");
     }
 }

8b9e9562eb156f209b8d28478da78ddceb76fc00
ShaQi <shaghay3gh@gmail.com>
2024-02-13 14:03:12 +0100

    now, the not so elegant code (print_a_piramid) can print the spaces and #'s the way the assiment's requires. but the more elegant code doesn't do it yet :|

--- piramid.c
+++ piramid.c
@@ -1,24 +1,25 @@
 #include "./src/cs50.h"
 #include <stdio.h>
 
+//void print_a_piramid(int n);
 void print_piramid(int n);
-void print_another_piramid(int n);
+void print_a_piramid(int n);
 
 
 int main(void){
     int i = get_int("piramid: ");
-    int j = get_int("second piramid: ");
-    print_another_piramid(j);
-    print_piramid(i);
+    print_a_piramid(i);
 }
 
 
-void print_piramid(int n){
+void print_a_piramid(int n){
     for (int i = 0; i < n; i++){
+        for (int v = (n-i); v > 0; v--){
+            printf(" ");
+        }
         for (int j = 0; j < (i+1); j++){
             printf("#");
         }
-
         printf("\n");
     }
 }
@@ -30,8 +31,15 @@ void print_row(int n){
     }
 }
 
-void print_another_piramid(int n){
-    for (int i; i < n; i++){
+void print_free_space(int n, int i){
+    for (int v = (n - i); v > 0; i--){
+        printf(" ");
+    }
+}
+
+void print_piramid(int n){
+    for (int i=0; i < n; i++){
+        print_free_space(n, i);
         print_row(i);
         printf("\n");
     }

d662be987ebecb7784402767eb8c86ba20b6b4c6
ShaQi <shaghay3gh@gmail.com>
2024-02-27 12:13:44 +0100

    playing with strings

---
+++ hi.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "./src/cs50.h"
+
+int main(void){
+    char c1 = 'H';
+    char c2 = 'I';
+    char c3 = '!';
+
+    string s = "HI!";
+    string t = "BYE!";
+
+    printf("%i %i %i\n", c1, c2, c3);
+    printf("%s\n", s);
+    printf("%c %c %c\n", s[0], s[1], s[2]);
+    printf("%i %i %i %i\n", s[0], s[1], s[2], s[3]);
+    printf("%i %i %i %i %i %i %i %i %i\n", t[0], t[1], t[2], t[3], t[4], t[5], t[6], t[7], t[8]);
+    printf("%i %i %i %i %i %i %i %i\n", s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7]);
+
+
+}

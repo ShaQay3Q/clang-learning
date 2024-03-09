a109ffe972140a51af17721565825910b3e1f332
ShaQayeQ <shaghay3gh@gmail.com>
2024-03-09 12:07:12 +0100

    week 2 - new way of filling up the array on double_up

--- double_up.c
+++ double_up.c
@@ -3,6 +3,7 @@
 
 int array_of_doubles(int array_length, int array_name[]);
 void print_array(int length, int name[]);
+int array_of_doubles02(int array_length, int array_name[]);
 
 int main(void){
     
@@ -15,7 +16,7 @@ int main(void){
     print_array(arrayLength, doubles);
 }
 
-int array_of_doubles(int array_length, int array_name[]){
+int array_of_doubles01(int array_length, int array_name[]){
 
     int n = 1;
     for (int i = 0; i < array_length; i++){
@@ -25,11 +26,23 @@ int array_of_doubles(int array_length, int array_name[]){
     return array_name[array_length];
 }
 
+// this one looks interesting.
+// I will try to impliment this style for solving similar problems later
+int array_of_doubles02(int array_length, int array_name[]){
+
+    array_name[0] = 1;
+    for (int i = 1; i < array_length; i++){
+        array_name[i] = array_name[i-1] * 2;
+    }
+    return array_name[array_length];
+}
+
+
 void print_array(int length, int name[]){
     
     // Seems this one is correct too! At least it works. WHY?
 
-    name[length] = array_of_doubles(length, name);
+    name[length] = array_of_doubles02(length, name);
     for (int i = 0; i < length; i++){
         printf("index %i: value %i\n", i, name[i]);
     }

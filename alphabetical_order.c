60d3b2a882911924084a6a28c8b1513ab8975623
ShaQayeQ <shaghay3gh@gmail.com>
2024-03-10 02:16:17 +0100

    Week 2 - func sort was added - alphabetical_order.c

--- alphabetical_order.c
+++ alphabetical_order.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 void swapChar(char *Str, int index1, int index2);
+void sort(char *str, int length);
 
 int main(void){
 
@@ -11,13 +12,16 @@ int main(void){
   int l = strlen(Str);
 
   while (*Str != *modStr) {
-    for (int i = 0; i < l-1; i++) {
-        printf("%c\n", Str[i]);
-      if (Str[i] > Str[i + 1]) {
-        swapChar(Str, i, (i+1));
-        printf("Str[i]: %c\nStr[i+1]: %c\n", Str[i], Str[i+1]);
-      }
-    }
+
+    sort(Str, l);
+
+    // for (int i = 0; i < l-1; i++) {
+    //     printf("%c\n", Str[i]);
+    //   if (Str[i] > Str[i + 1]) {
+    //     swapChar(Str, i, (i+1));
+    //     printf("Str[i]: %c\nStr[i+1]: %c\n", Str[i], Str[i+1]);
+    //   }
+    // }
   }
   printf("\nStr: %s\nmodStr: %s\n", Str, modStr);
 }
@@ -27,4 +31,14 @@ void swapChar(char *Str, int index1, int index2)
     char temp = Str[index1];
     Str[index1] = Str[index2];
     Str[index2] = temp;
+}
+
+void sort(char *str, int length){
+    
+    for (int i = 0; i < length-1; i++) {
+      if (str[i] > str[i + 1]) {
+        swapChar(str, i, (i+1));
+      }
+    }
+
 }

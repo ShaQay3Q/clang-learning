bac6f822d4812018221c7d034ea07b5c56d0bd1a
ShaQay3Q <shaghay3gh@gmail.com>
2024-05-06 17:47:23 +0200

    getting rid of an extra condition

--- sort-insertion.c
+++ sort-insertion.c
@@ -4,36 +4,36 @@ void swap(int *intArr, int i, int j);
 void printDigits(int *Arr, int length);
 void insertation_sort(int *intArr, int length);
 
-int main(void){
-    int initArr[] = {2, 8, 5, 3, 9, 4, 1};
-    int arrLength = 7;
+int main(void) {
+  int initArr[] = {2, 8, 5, 3, 9, 4, 1};
+  int arrLength = 7;
 
-    insertation_sort(initArr, arrLength);
+  insertation_sort(initArr, arrLength);
 
-    printf("Insert sort: ");
-    printDigits(initArr, arrLength);
+  printf("Insert sort: ");
+  printDigits(initArr, arrLength);
 }
 
-void swap(int *intArr, int i, int iMin){
-    int temp = intArr[iMin];
-    intArr[iMin] = intArr[i];
-    intArr[i] = temp;
+void swap(int *intArr, int i, int iMin) {
+  int temp = intArr[iMin];
+  intArr[iMin] = intArr[i];
+  intArr[i] = temp;
 }
 
-void printDigits(int *Arr, int length){
-    
-        for (int i = 0; i < length; i++){
-        printf("%i", Arr[i]);
+void printDigits(int *Arr, int length) {
+
+  for (int i = 0; i < length; i++) {
+    printf("%i", Arr[i]);
+  }
+  printf("\n");
+}
+
+void insertation_sort(int *intArr, int length) {
+  for (int i = 0; i < length; i++) {
+    for (int j = i; j > 0; j--) {
+      if (intArr[j - 1] > intArr[j]) {
+        swap(intArr, j, j - 1);
+      }
     }
-    printf("\n");
-}
-
-void insertation_sort(int *intArr, int length){
-    for (int i = 0; i < length; i++){
-        for (int j = i; j > 0; j--){
-            if (j > 0 && intArr[j-1] > intArr[j]){
-                swap(intArr, j, j-1);
-            }
-        }
-    }   
+  }
 }

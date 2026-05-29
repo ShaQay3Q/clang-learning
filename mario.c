29896358af8b9c46d446816e1d13f9e578faed17
ShaQay3Q <shaghay3gh@gmail.com>
2026-05-29 23:31:46 +0200

    mario.c return values

--- mario.c
+++ mario.c
@@ -8,28 +8,31 @@ void print_row(int size);
 
 
 int main(int argc, string argv[]) {
-
     if (argc == 1){
         printf("Not enough arguments!\nFollow: ./mario <number>\n");
         return 1;
     }
     int n = atoi(argv[1]);
 
-        if (n == 0) {
+    if(n < 0)
+    {
+        printf("Realy?!\n");
+        return 1;
+    } else if (n == 0)
+    {
+        printf("Come on! Do you really wanna see no blocks at all!\n");
+        return 1;
+    } else {
 
-            printf("Come on! Do you really wanna see no blocks at all!\n");
-
-        } else {
-
-            print_grid(n);
-        }
-        return 0;
+        print_grid(n);
+    }
+    return 0;
 }
 
 // print_grid prints grid of bricks
 void print_grid(int size) {
     for (int i = 0; i < size; i++) {
-        print_row(size);
+        print_row(i + 1);
     }
 }
 

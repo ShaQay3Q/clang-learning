feb5fca49dcf1ecbd55d958b94ed074924856f9b
ShaQay3Q <shaghay3gh@gmail.com>
2026-05-29 19:55:52 +0200

    more exmaples with argc and argv[]-great

--- great.c
+++ great.c
@@ -7,7 +7,10 @@ int main(int argc, string argv[])
     if (argc == 1 ) {
         printf("Usage: ./greet name\n");    
     } else {
-        printf("Hello, %s!\n", argv[1]);
+        for(int i = 0; i < argc; i++){
+        printf("Hello, %s!\n", argv[i]);
+
+        }
     }
     for(int i = 0; i < argc; i++){
         printf("argv[%i] -> %s\n", i, argv[i]);

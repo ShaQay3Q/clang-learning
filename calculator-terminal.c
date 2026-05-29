d5e9432de5b789393639a4f4e034072d79d86947
ShaQay3Q <shaghay3gh@gmail.com>
2026-05-29 20:28:56 +0200

    use of argc and argv[] in caclulator program

---
+++ calculator-terminal.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if(argc != 4)
+    {
+        printf("Usage: ./calc number operator number\n");
+        return 1;
+    }
+
+    double a = atof(argv[1]);
+    double b = atof(argv[3]);
+    double c;
+    char operator = (char)argv[2][0];
+
+    if (operator == '+')
+    { c =  a + b;}
+    else if (operator == '-') {
+        c = a - b ;
+    } else if(operator == '*'){
+        c = a * b;
+    } else {
+        c =  a / b;
+    }
+
+    printf("%g %c %g = %g\n", a, operator, b, c);
+}

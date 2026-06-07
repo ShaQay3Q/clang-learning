67f1119230af9bf071298871d9454da77e00681a
ShaQay3Q <shaghay3gh@gmail.com>
2026-06-07 23:19:35 +0200

    initials.c - comment change

--- initials.c
+++ initials.c
@@ -3,6 +3,11 @@
 
 int main(int argc, char* argv[5])
 {
+    if (argc < 2)
+    {
+        printf("Usage: ./initials Fistname Lastname\n");
+        return 0;
+    }
     for(int i = 1; i < argc; i++)
     {
         printf("%c", toupper(argv[i][0]));

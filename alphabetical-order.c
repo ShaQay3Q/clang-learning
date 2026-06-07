82c6edf1858ceebb7a9c740e29cf4271c52518bf
ShaQay3Q <shaghay3gh@gmail.com>
2026-06-07 14:27:04 +0200

    alphabetical-order.c - improvements

--- alphabetical-order.c
+++ alphabetical-order.c
@@ -13,25 +13,24 @@ int main(void)
     string input = get_string("Enter your input in all lowcase letters: ");
     int counter = 0, l = strlen(input);
 
-    for (int i = 0; i < l; i++)
+    for (int i = 0; i < l-1; i++)
     {
-        if(compare(input[i], input[i+1])){
+        if(compare(input[i], input[i+1]))
+        {
             counter++;
         }   
     }
-    if (counter == l - 1) {
+    if (counter == l - 1)
+    {
         printf("YES\n");
         return 0;
     }
-        printf("NO\n");
-        return 1;
+    printf("NO\n");
+    return 1;
+
 }
 
 bool compare(char c1, char c2)
 {
-    if(c1 < c2)
-    {
-        return true;
-    }
-    return false;
+    return c1 < c2;
 }

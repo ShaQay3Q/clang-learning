065ff137df42199f01188d2909c6f1190bdaf7dd
ShaQay3Q <shaghay3gh@gmail.com>
2026-06-10 13:49:26 +0200

    linear-search.c - more interactive

--- linear_search.c
+++ linear_search.c
@@ -3,16 +3,17 @@
 
 int main(void)
 {
-    int array[] = {1, 4, 3, 6, 9, 10, 12, 5, 7, 8, 2, 11};
-    int length = (int)sizeof(array)/4; // sizeof() -> runtime -> gives the size in bytes ==> /4 is needed
+    int n = get_int("Search for number: ");
+    int numbers[] = {1, 4, 3, 6, 9, 10, 12, 5, 7, 8, 2, 11};
+    int length = (int)sizeof(numbers)/4; // sizeof() -> runtime -> gives the size in bytes ==> /4 is needed
     for(int i = 0; i < length-1; i++)
     {
-        if (array[i] == 5)
+        if (numbers[i] == n)
         {
-            printf("array[%i] has the value of 2\n", i);
+            printf("array[%d] has the value of %d\n", i, n);
             return 0;
         }
     }
-    printf("2 was not found!\n");
+    printf("%i was not found!\n", n);
     return 1;
 }

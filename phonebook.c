31047b7fbfc0fa92433e4b217637440723a433b7
ShaQay3Q <shaghay3gh@gmail.com>
2026-06-11 21:22:04 +0200

    phonebook.c created

--- phonebook.c
+++ phonebook.c
@@ -1,9 +1,24 @@
-#include "./src//cs50.h"
+#include "./src/cs50.h"
 #include <string.h>
 #include <stdio.h>
 
 int main(void)
 {
-    string names = {"Ensi", "Aida", "Maryam", "Anran", "Natia"};
-    int numbers = {7, 10, 13, 1, 20};
+    string names[] = {"Aida", "Anran", "Ensi", "Maryam", "Natia"};
+    int numbers[] = {7, 10, 13, 1, 20};
+
+    string name = get_string("Name: ");
+
+    for(int i = 0; i < 5; i++)
+    {
+        printf("%s\n", names[i]);
+        if(strcmp(names[i], name) == 0)
+        {
+            printf("%s number is %i\n", name, numbers[i]);
+            return 0;
+        }
+
+    }
+    printf("%s Not Found!\n", name);
+    return 1;
 }

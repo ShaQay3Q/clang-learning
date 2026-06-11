1714998cf4b98885ab1efd805d7ab0da4504e6ba
ShaQay3Q <shaghay3gh@gmail.com>
2026-06-11 20:00:16 +0200

    phonebook.c created

---
+++ phonebook.c
@@ -0,0 +1,9 @@
+#include "./src//cs50.h"
+#include <string.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string names = {"Ensi", "Aida", "Maryam", "Anran", "Natia"};
+    int numbers = {7, 10, 13, 1, 20};
+}

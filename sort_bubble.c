4f3bd75ceca5389d4cf86614c12548b32e58c90e
ShaQay3Q <shaghay3gh@gmail.com>
2026-06-15 18:04:10 +0200

    iteration.c - start with loop, now is about to swtich to recursion

--- sort_bubble.c
+++ sort_bubble.c
@@ -8,8 +8,8 @@ void bubble_sort(int *numbers, int length);
 
 int main(void)
 {
-    int numbers[] = {2, 8, 5, 3, 9, 4, 1};
-    // int numbers[] = {5, 3, 4, 1};
+    // int numbers[] = {2, 8, 5, 3, 9, 4, 1};
+    int numbers[] = {5, 3, 4, 1};
     int length = sizeof(numbers) / sizeof(numbers[0]);
     bubble_sort(numbers, length);
     printDigits(numbers, length);
@@ -36,19 +36,24 @@ void swap(int *numbers, int i, int iMin)
 void bubble_sort(int *numbers, int length)
 {    
     bool swapped;
+    printDigits(numbers, length);
 
     for(int i = 0; i < length - 1; i++)
     {
-    swapped = false;
+        swapped = false;
+        printf("i: %i\n", i);
 
-    for(int j = 0; j < length - i - 1; j++)
-    {
-        if(numbers[j] > numbers[j + 1])
+        for(int j = 0; j < length - i - 1; j++)
         {
-            swap(numbers, j, j+1);
-            swapped = true;
+            printf("j: %i\n", j);
+
+            if(numbers[j] > numbers[j + 1])
+            {
+                swap(numbers, j, j+1);
+                swapped = true;
+            }
+            printDigits(numbers, length);
         }
-    }
     if(!swapped)
         {
             break;

339b1d9255cced2802d088465b34233bf6276840
ShaQay3Q <shaghay3gh@gmail.com>
2026-06-17 11:30:08 +0200

    devide the input array into 2 subsets

--- sort_merge.c
+++ sort_merge.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 void merge_sort(int numbers[], int length);
+void printDigits(int *input, int length);
 
 
 int main(void)
@@ -9,22 +10,40 @@ int main(void)
     int numbers[] = {5, 3, 4, 1};
     int length = sizeof(numbers) / sizeof(numbers[0]);
 
-
+    if (length == 0)        
+    {
+        printf("No Exiting Set of Numbers!\n");
+    }
     merge_sort(numbers, length);
 }
 
 void merge_sort(int *numbers, int length)
 {
-        if (length == 0)
-        {
-            printf("No Exiting Set of Numbers!\n");
-        }
-        if (length == 1)
-        {
-            printf("%i", numbers[0]);
-        }
+    int subleft[length/2];
+    int subright[length/2];
+    for(int i = 0; i < (length/2 + 1); i++)
+    {
+        subleft[i] = numbers[i];
+    }
+    for(int j = 0; j < (length/2 + 1); j++)
+    {
+        subright[j] = numbers[length/2 + j];
+    }
+    printf("SubLeft: ");
+    printDigits(subleft, length/2);
+
+    printf("SubRight: ");
+    printDigits(subright, length/2);
+}
+
+bool compare(int n1, int n2)
+{
+    return n1 < n2;
+}
+
+void printDigits(int *input, int length)
+{
         for (int i = 0; i < length; i++)
-        {
-            merge_sort(numbers, length);
-        }
+            printf("%i", input[i]);
+    printf("\n");
 }

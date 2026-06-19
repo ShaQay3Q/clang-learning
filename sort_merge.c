50b86cb00cfc7ba7eb048c04216bf358999740c0
ShaQay3Q <shaghay3gh@gmail.com>
2026-06-19 23:42:10 +0200

    starts with a using indices

--- sort_merge.c
+++ sort_merge.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 
-void merge_sort(int *numbers, int length, int depth);
+void merge_sort(int *numbers, int left, int right);
 void print_array(const int *input, int length);
 void merge_two_arrays(int *subleft, int *subright, int subleft_length, int subright_length, int *numbers);
 
@@ -13,15 +13,16 @@ int main(void)
     int numbers[] = {-5, 7, -2, 10, 3};
     // int numbers[] = {999};
     int length = sizeof(numbers) / sizeof(numbers[0]);
-    int depth = 1;
+    `
 
-    // assert(merge_two_arrays_sort(numbers, length), numbers[] == {999});
-    merge_sort(numbers, length, depth);
+    merge_sort(numbers, left, depth);
     print_array(numbers, length);
 }
 
-void merge_sort(int *numbers, int length, int depth)
-{         
+void merge_sort(int *numbers, int left, int right)
+{  
+    int length = right - left + 1;
+    int depth = 1;      
     for(int i = 0; i < depth; i++)
         printf("  ");
 
@@ -43,7 +44,7 @@ void merge_sort(int *numbers, int length, int depth)
     int subright[length_right];
 
     memcpy(subleft, numbers, length_left * sizeof(subleft[0]));
-    
+
     for (int i = 0; i < length_left; i++)
         subleft[i] = numbers[i];
     merge_sort(subleft, length_left, depth);

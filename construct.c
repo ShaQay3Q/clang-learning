1aa1097e67bc626909f19d56aa620d430a00681f
ShaQay3Q <shaghay3gh@gmail.com>
2026-06-27 22:05:58 +0200

    array of structs

--- construct.c
+++ construct.c
@@ -1,5 +1,6 @@
 #include "./src/cs50.h"
 #include <stdio.h>
+#include<string.h>
 
 typedef struct
 {
@@ -31,6 +32,7 @@ candidate deputy =
 //! The compiler treats them completely differently.
 
 candidate get_candidate(void);
+void print_candidates(candidate candidates[],int length);
 
 
 int main(void)
@@ -39,24 +41,33 @@ int main(void)
     // A statement is an instruction
     // (tells the program to do something.
     // -> These are actions that happen when the program runs.)
-    president.name = "Alice";
-    president.votes = 13;
+    // president.name = "Alice";
+    // president.votes = 13;
 
-    candidate user_input =
+    candidate candidates[3];
+    // candidate user_input =
+    // {
+    //     .name = get_string("Candidate name: "),
+    //     .votes = get_int("Number of votes: ")
+    // };
+
+    // printf("%s won the election with %i votes.\n",
+    //     president.name, president.votes);
+
+    // printf("%s has %i votes.\n",
+    //     user_input.name, user_input.votes);
+
+    for (int i = 0; i < 3; i++)
     {
-        .name = get_string("Candidate name: "),
-        .votes = get_int("Number of votes: ")
-    };
+        candidates[i] = get_candidate();
+    }
 
-    printf("%s won the election with %i votes.\n",
-        president.name, president.votes);
 
-    printf("%s has %i votes.\n",
-        user_input.name, user_input.votes);
+    // candidate new_candidate = get_candidate();
+    // printf("%s has %i votes.\n",
+    //     new_candidate.name, new_candidate.votes);
 
-    candidate new_candidate = get_candidate();
-    printf("%s has %i votes.\n",
-        new_candidate.name, new_candidate.votes);
+    print_candidates(candidates, 3);
 }
 
 // Function to get a new_candidate candidate
@@ -79,4 +90,11 @@ candidate get_candidate(void)
     // return new_candidate;
 }
 
-
+void print_candidates(candidate candidates[],int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        printf("Candidate %i: %s has %i votes\n",
+            i+1, candidates[i].name, candidates[i].votes);
+    }
+}

2ceb67202aec0b1e095702454e5a7799c5055966
ShaQay3Q <shaghay3gh@gmail.com>
2026-07-01 00:57:46 +0200

    fibonacci-sequence.c - completed - three recursion

--- fibonacci-sequence.c
+++ fibonacci-sequence.c
@@ -1,16 +1,31 @@
 #include "./src/cs50.h"
 #include <stdio.h>
 
+int fbncc_sqnc(int n);
+
+
 int main(void)
 {
+    int n = get_int("Fibonacci sequence of: ");
+
+    printf("is: %i\n", fbncc_sqnc(n));
 
 }
 
+//! Fibonacci Sequence is a a TREE RECURSION
+//! wherase the Factorial is a lINEAR RECURSION
 int fbncc_sqnc(int n)
 {
+    // Error case
+    if (n < 0)
+        return -1;
     // Base case
+    if (n == 0)
+        return 0;
 
+    if (n == 1)
+        return 1;
 
     // Recursive case
-    
+    return fbncc_sqnc(n - 1) + fbncc_sqnc(n - 2);
 }

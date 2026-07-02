9744398f7a1223cdabbf3b05ad6c34f7f53704d9
ShaQay3Q <shaghay3gh@gmail.com>
2026-07-02 13:01:50 +0200

    fibonacci-sequence.c - linear recursion

--- fibonacci-sequence.c
+++ fibonacci-sequence.c
@@ -4,20 +4,23 @@
 #include <stdio.h>
 
 bool fbncc_sqnc(int n, int *result);
+int fibonacci(int x);
+int fib(int prev, int curr, int steps);
 
 
 int main(void)
 {
-    int result;
+    int n = get_int("Fibonacci number: ");
+    // int result;
 
-    if (!fbncc_sqnc(get_int("Fibonacci sequence of: "), &result))
-    {
-        printf("Invalid input\n");
-        return 1;
-    }
+    // if (!fbncc_sqnc(n, &result))
+    // {
+    //     printf("Invalid input\n");
+    //     return 1;
+    // }
 
-    printf("is: %i\n", result);
-    return 0;
+    // printf("is: %i\n", result);
+    printf("Fibonnaci number of %i, is %i\n", n, fibonacci(n));
 
 }
 
@@ -36,13 +39,28 @@ bool fbncc_sqnc(int n, int *result)
     }
 
     // recursive part
-    int a;
-    int b;
+    int a, b;
 
-    if (!fbncc_sqnc(n-1, &a)) return false;
+    fbncc_sqnc(n-1, &a);
+    fbncc_sqnc(n-2, &b);
     
-    if (!fbncc_sqnc(n-2, &b)) return false;
-    
-    *result = a + b;
+    *result = a + b ;
     return true;
+}
+
+// Wrapper function
+int fibonacci(int x)
+{
+    if (x == 0)
+        return x;
+    return fib(0, 1, x-1);
+}
+
+// Recursive function
+int fib(int prev, int curr, int steps)
+{
+    if (steps == 0)
+        return curr;
+
+    return fib(curr, prev + curr, steps - 1);
 }

d6d80a188b087d744410ad1950e9bf744fd94856
ShaQay3Q <shaghay3gh@gmail.com>
2026-07-22 21:03:19 +0200

    week 4 -  cs50 - pointers - this shit is confusing

--- addresses.c
+++ addresses.c
@@ -3,7 +3,8 @@
 int main(void)
 {
     int n = 50;
-    int *p = &n;
+    int *p = &n; // give me the address if n
     printf("%i\n", n);
     printf("%p\n", &n); // %p => is for pointer
+    printf("%i\n", *p);
 }

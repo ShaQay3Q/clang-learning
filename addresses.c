4c392704047323403d6a3a3bfcf9ab68d01f966a
ShaQay3Q <shaghay3gh@gmail.com>
2026-08-08 13:39:37 +0200

    doing arithmatics on s

--- addresses.c
+++ addresses.c
@@ -11,12 +11,17 @@ int main(void)
     // printf("%p\n", &n); // %p => is for pointer
     // printf("%i\n", *p); //! Dereference operator
 
-    string s = "HI!";
+    char *s = "HI!";
     printf("%s\n", s);
-    printf("%p\n", s);
-    printf("%p\n", &s[0]);
-    printf("%p\n", &s[1]);
-    printf("%p\n", &s[2]);
-    printf("%p\n", &s[3]);
+    printf("%p\n", &s);
+
+    printf("%c\n", *s); // => s[0]
+    printf("%c\n", *(s+1)); // => s[1]
+    printf("%c\n", *(s+2)); // => s[2]
+
+
+    printf("%c\n", s[1]);
+    printf("%c\n", s[2]);
+
 
 }

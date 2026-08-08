6f8b4bdc1be58c20eec4b6ba7174e6d1be3b1418
ShaQay3Q <shaghay3gh@gmail.com>
2026-08-08 20:02:00 +0200

    looking under the hood

--- copy.c
+++ copy.c
@@ -1,8 +1,21 @@
 #include "./src/cs50.h"
 #include <stdio.h>
+#include <ctype.h>
 #include <string.h>
 
 int main(void)
 {
-    
+    char *s = get_string("s: ");
+    char *t = s; // t is the copy of the address of the string which s holds
+
+    printf("s is %s\n", s);
+    printf("t is %s\n", t);
+
+    printf("%p: \n", &s);
+    printf("%p: \n", &t);
+
+    t[0] = toupper(*t);
+    printf("t: %s\n", t);
+    printf("s: %s\n", s);
+
 }

a362ea55439a177d096f3b4e78ebb71e3c5a8e06
ShaQay3Q <shaghay3gh@gmail.com>
2026-08-08 13:26:34 +0200

    let's simplify

--- addresses.c
+++ addresses.c
@@ -1,11 +1,22 @@
+#include "./src/cs50.h"
+#include <inttypes.h>
 #include <stdio.h>
 
 int main(void)
 {
-    int n = 50;
-    int *p = &n; // give me the address if n
+    // int n = 50;
+    // int *p = &n; // give me the address if n
     // printf("%i\n", n);
-    printf("%p\n", p);
+    // printf("%p\n", p);
     // printf("%p\n", &n); // %p => is for pointer
-    printf("%i\n", *p); //! Dereference operator
+    // printf("%i\n", *p); //! Dereference operator
+
+    string s = "HI!";
+    printf("%s\n", s);
+    printf("%p\n", s);
+    printf("%p\n", &s[0]);
+    printf("%p\n", &s[1]);
+    printf("%p\n", &s[2]);
+    printf("%p\n", &s[3]);
+
 }

1e16186cc1511c3d4cf95603aa61bca9c39178a4
ShaQay3Q <shaghay3gh@gmail.com>
2026-08-08 13:06:54 +0200

    starting with strings and training wheels

--- addresses.c
+++ addresses.c
@@ -4,7 +4,8 @@ int main(void)
 {
     int n = 50;
     int *p = &n; // give me the address if n
-    printf("%i\n", n);
-    printf("%p\n", &n); // %p => is for pointer
-    printf("%i\n", *p);
+    // printf("%i\n", n);
+    printf("%p\n", p);
+    // printf("%p\n", &n); // %p => is for pointer
+    printf("%i\n", *p); //! Dereference operator
 }

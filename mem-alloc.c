5efc663be2649ec668e20370da10de30160d95e1
ShaQay3Q <shaghay3gh@gmail.com>
2026-08-20 14:46:05 +0200

    memory allocation - pointers are fucking confusing

--- mem-alloc.c
+++ mem-alloc.c
@@ -7,8 +7,16 @@
 int main(void)
 {
     char *s = get_string("s: ");
-    char *t = malloc(4);
+    int l = strlen(s);
+
+    char *t = malloc(l + 1); // length s + extra bite for \0 char
     // printf("s is %s\n", s);
+    for (int i =0; i < l; i++)
+    {
+        t[i] = s[i];
+    }
+
+    t[0] = toupper(*t);
     printf("s is %s\n", s);
-    printf("t is %p\n", t);
+    printf("t is %s\n", t);
 }

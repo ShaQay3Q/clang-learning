876762bada1d95456be16a1680071654ed9ee6dc
ShaQay3Q <shaghay3gh@gmail.com>
2026-08-22 16:28:02 +0200

    stream - using fprintf() to decide where to output data

--- new-phonebook.c
+++ new-phonebook.c
@@ -5,12 +5,19 @@
 int main(void)
 {
     FILE *file = fopen("phonebook.csv", "w"); // "w" for write
-
+    if (file == NULL)
+    {
+        return 1;
+    }
     char *name = get_string("Name: ");
     char *number = get_string("Number: ");
 
     fprintf(file, "%s, %s\n", name, number);
-
+    
+    // write on therminal!
+    fprintf(stdout, "DONE!\n"); 
+    
+    // close the open file, clearup the allocated memory
     fclose(file);
     return 0;
 }

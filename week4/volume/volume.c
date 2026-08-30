61fa9da3c2814c7a08ad8b4d6b5f166f661c5651
ShaQay3Q <shaghay3gh@gmail.com>
2026-08-30 17:39:38 +0200

    check argv[1] and argv[2], now wanna validate argv[3]

--- week4/volume/volume.c
+++ week4/volume/volume.c
@@ -2,28 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 
 bool has_same_format(char const *input, char const *format);
+bool is_filename_acceptable(char const *input);
+
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
+    // Check for correct number of arguments
+    if (argc != 4)
     {
-        printf("Usage: %s *.wav\n", argv[0]);
+        printf("Usage: %s input.wav output.wav factor\n", argv[0]);
         return 1;
     }
 
-    if (strlen(argv[1]) < 5)
+    // Check the input and outout name and format
+    if (!is_filename_acceptable(argv[1]) || !is_filename_acceptable(argv[2]))
     {
-        printf("Error: filename must contain at least one character before the .wav extension.\n");
         return 1;
     }
 
-    if (!has_same_format(argv[1], ".wav"))
-    {
-        printf("Error: file must have a .wav extension.\n");
-        return 1;
-    }
+    // convert the factor into float
+    char *end;
+    float f = strtof(argv[3], &end);
+    fprintf(stdout, "%f\n",  f);
+
+
+    FILE *file = fopen(argv[1], "rb");
+    if (file == NULL) return 1;
+    
+
+
 
     return 0;
 
@@ -44,4 +54,22 @@ bool has_same_format(char const *input, char const *format)
     if (strcmp(input + (szinput - szformat), format) != 0) return false;
     
     return true; // YES
+}
+
+bool is_filename_acceptable(char const *input)
+{
+        if (strlen(input) < 5)
+    {
+        printf("Error: filename must contain at least one character before the .wav extension.\n");
+        return false;
+    }
+
+    // compare for correct format
+    if (!has_same_format(input, ".wav"))
+    {
+        printf("Error: input file must have a .wav extension.\n");
+        return false;
+    }
+
+    return true;
 }

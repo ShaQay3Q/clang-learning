6a4ac892928245cf490e4d449a4cb69edda08b37
ShaQay3Q <shaghay3gh@gmail.com>
2026-08-31 01:24:40 +0200

    reflect() implemented

--- week4/filter/filter-more/helpers.c
+++ week4/filter/filter-more/helpers.c
@@ -21,6 +21,16 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width / 2; j++)
+        {
+            RGBTRIPLE pixel;
+            pixel = image[i][j];
+            image[i][j] = image[i][width - j - 1];
+            image[i][width - j - 1] = pixel;            
+        }
+    }
     return;
 }
 

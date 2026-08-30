aabac23df75e67608e43e2e8189bf73bf49f6619
ShaQay3Q <shaghay3gh@gmail.com>
2026-08-31 00:58:07 +0200

    greyscale() implemented

---
+++ week4/filter/filter-more/helpers.c
@@ -0,0 +1,37 @@
+#include "helpers.h"
+#include <math.h>
+
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int avrage = round(((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0));
+            image[i][j].rgbtBlue = avrage;
+            image[i][j].rgbtGreen = avrage;
+            image[i][j].rgbtRed = avrage;
+        }
+    }
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}

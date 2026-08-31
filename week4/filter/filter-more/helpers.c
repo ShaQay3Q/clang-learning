62a8931242d56675a4e070239659f534806efdc4
ShaQay3Q <shaghay3gh@gmail.com>
2026-08-31 02:49:51 +0200

    blure() implemented

--- week4/filter/filter-more/helpers.c
+++ week4/filter/filter-more/helpers.c
@@ -1,5 +1,7 @@
 #include "helpers.h"
 #include <math.h>
+#include <stdlib.h>
+
 
 
 // Convert image to grayscale
@@ -37,6 +39,54 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            RGBTRIPLE pixel;
+
+            int sum_b = 0;
+            int sum_g = 0;
+            int sum_r = 0;
+            int count = 0;
+
+            // di/dj = movement
+            // i + di / j + dj = actual neighbor coordinate
+            for (int di = -1; di <= 1; di++)
+            {
+                for (int dj = -1; dj <= 1; dj++)
+                {
+                    if (0 <= i + di && i + di < height)
+                    {
+                        if (0 <= j + dj && j + dj < width)
+                        {
+                            sum_b = sum_b + image[i + di][j + dj].rgbtBlue;
+                            sum_g = sum_g + image[i + di][j + dj].rgbtGreen;
+                            sum_r = sum_r + image[i + di][j + dj].rgbtRed;
+
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            pixel.rgbtBlue = round((float)sum_b / count);
+            pixel.rgbtGreen = round((float)sum_g / count);
+            pixel.rgbtRed = round((float)sum_r / count);
+
+            copy[i][j] = pixel;
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = copy[i][j];
+        }
+    }
+    
     return;
 }
 

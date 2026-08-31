0c9d7e38b5eaed401784bf3cf05c04560a8d8390
ShaQay3Q <shaghay3gh@gmail.com>
2026-08-31 21:20:19 +0200

    edge() implemented

--- week4/filter/filter-more/helpers.c
+++ week4/filter/filter-more/helpers.c
@@ -93,5 +93,59 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE copy[height][width];
+    int X[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    int Y[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            RGBTRIPLE px;
+
+            int s_red[2] = {0, 0};
+            int s_green[2] = {0, 0};
+            int s_blue[2] = {0, 0};
+
+            for (int di = -1; di <= 1; di++)
+            {
+                for (int dj = -1; dj <= 1; dj++)
+                {
+                    if (0 <= i + di && i + di < height)
+                    {
+                        if (0 <= j + dj && j + dj < width)
+                        {
+                            s_blue[0] = (s_blue[0] + image[i + di][j + dj].rgbtBlue * X[di+1][dj+1]);
+                            s_blue[1] = (s_blue[1] + image[i + di][j + dj].rgbtBlue * Y[di+1][dj+1]);
+                            s_green[0] = (s_green[0] + image[i + di][j + dj].rgbtGreen * X[di+1][dj+1]);
+                            s_green[1] = (s_green[1] + image[i + di][j + dj].rgbtGreen * Y[di+1][dj+1]);
+                            s_red[0] = (s_red[0] + image[i + di][j + dj].rgbtRed * X[di+1][dj+1]);
+                            s_red[1] = (s_red[1] + image[i + di][j + dj].rgbtRed * Y[di+1][dj+1]);
+
+                        }
+                    }
+                }
+            }
+
+            int blue = round(sqrt((s_blue[0] * s_blue[0]) + s_blue[1] * s_blue[1]));
+            int green = round(sqrt((s_green[0] * s_green[0]) + s_green[1] * s_green[1]));
+            int red = round(sqrt((s_red[0] * s_red[0]) + s_red[1] * s_red[1]));
+
+            px.rgbtBlue = blue > 255 ? 255 : blue;
+            px.rgbtGreen = green > 255 ? 255 : green;
+            px.rgbtRed = red > 255 ? 255 : red;
+
+            copy[i][j] = px;
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = copy[i][j];
+        }
+    }
+
     return;
 }

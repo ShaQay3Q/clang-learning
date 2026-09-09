e6bf6419286e736c8e88a967fae7404f4524c7e9
ShaQay3Q <shaghay3gh@gmail.com>
2026-09-09 12:24:06 +0200

    add - to signature

--- week4/is_PDF.c
+++ week4/is_PDF.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
         return 2;
     }
 
-    const uint8_t signature[4] = {0x25, 0x50, 0x44, 0x46};
+    const uint8_t signature[5] = {0x25, 0x50, 0x44, 0x46, 0x2D};
     uint8_t header[1024];
     size_t buffer;
 
@@ -33,11 +33,11 @@ int main(int argc, char *argv[])
 
     buffer = fread(header, 1, sizeof(header), input);
 
-    for (size_t i = 0; i + 4 < buffer; i++)
+    for (size_t i = 0; i + 5 < buffer; i++)
     {
         // memcmp() compares the contents of two regions of memory, byte by byte.
         // compares a slice of memory with range of 5 to the array
-        if ((memcmp(signature, header + i, 4)) == 0)
+        if ((memcmp(signature, header + i, 5)) == 0)
         {
             fprintf(stdout, "%s is a PDF file\n", argv[1]);        
             fclose(input);
